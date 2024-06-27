dda0b61d3be9599ed9c9fc2882957433bfd65d83
codingNeurosis <valeeva.anastasiia.hennish@rambler.ru>
2024-06-27 15:29:54 +0800

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
-pip3 install aiofiles
-pip3 install websockets_proxy
-pip3 install loguru
-nohup python3 batch.py 0 >> logs.txt &
+#!/bin/bash
+# pip3 install aiofiles
+# pip3 install websockets_proxy
+# pip3 install loguru
+
+mkdir -p logs
+for i in {0..99}
+do
+    pm2 start --name "batch-$i" "python3 batch.py $i"
+done
+
+# kill -9 $(ps aux | grep "batch.py" | tr -s ' '| cut -d ' ' -f 2)
